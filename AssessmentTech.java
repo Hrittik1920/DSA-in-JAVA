35f09151c2d5fe94a5fbb8de03c8a3a195eb92c5
Hrittik Singh <hrittiksingh1920@gmail.com>
2025-04-02 21:55:59 +0530

    Log2 function added

--- AssessmentTech.java
+++ AssessmentTech.java
@@ -834,26 +834,26 @@ public class AssessmentTech {
     //     }
     //     return sum == a;
     // }
-    public static int log2(int bits) {
-        int log = 0;
-        if((bits & 0xffff0000) != 0) {
-            bits >>>= 16;
-            log += 16;
-        } 
-        if(bits >= 256) {
-            bits >>>= 8;
-            log += 8;
-        }
-        if(bits >= 16) {
-            bits >>>= 4;
-            log += 4;
-        }
-        if(bits >= 4) {
-            bits >>>= 2;
-            log += 2;
-        }
-        return log + (bits >>> 1);
-    }
+    // public static int log2(int bits) {  // As there is no log2 fuction in java.lang.Math class
+    //     int log = 0;
+    //     if((bits & 0xffff0000) != 0) {
+    //         bits >>>= 16;
+    //         log += 16;
+    //     } 
+    //     if(bits >= 256) {
+    //         bits >>>= 8;
+    //         log += 8;
+    //     }
+    //     if(bits >= 16) {
+    //         bits >>>= 4;
+    //         log += 4;
+    //     }
+    //     if(bits >= 4) {
+    //         bits >>>= 2;
+    //         log += 2;
+    //     }
+    //     return log + (bits >>> 1);
+    // }
     // public static int reverseNumber(int a) {
     //     int reverse = 0;
     //     while(a != 0) {
